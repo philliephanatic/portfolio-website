e3a3dae3789425522a63b8ff2973e14e42e0e9d3
philliephanatic <nolan.r.young@gmail.com>
2025-04-24 17:52:45 -0400

    Created SwiperJS slider in index.js, added homepage.css, carousel.css, layout.css

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
+import 'swiper/swiper-bundle.css';
+import './styles/carousel.css';
+
+Swiper.use([Autoplay]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 30,
+  allowTouchMove: false,
+  speed: 2000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    // when window width is >= 0px
+    0: {
+        slidesPerView: 2,
+    },
+    // when window width is >= 601px
+    601: {
+        slidesPerView: 3,
+    },
+    // when window width is >= 1440px
+    1440: {
+        slidesPerView: 4,
+    }
+  }
+});
